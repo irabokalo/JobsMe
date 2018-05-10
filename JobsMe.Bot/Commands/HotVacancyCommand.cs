2b649710b8290dfd4945b2eb48be56d415fac9b8
Ірина Бокало <iraklepak@gmail.com>
2018-05-11 00:09:14 +0300

    Db configuration

--- JobsMe.Bot/Commands/HotVacancyCommand.cs
+++ JobsMe.Bot/Commands/HotVacancyCommand.cs
@@ -1,6 +1,7 @@
 ﻿using JobsMe.BotApp.Commands;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using Telegram.Bot;
@@ -8,7 +9,7 @@ using Telegram.Bot.Types;
 
 namespace JobsMe.Bot.Commands
 {
-    public class HotVacancyCommand:Command
+    public class HotVacancyCommand : Command
     {
         public override string Name => "Hot Vacancy in company";
 
@@ -17,11 +18,11 @@ namespace JobsMe.Bot.Commands
             var chatId = message.Chat.Id;
             var messageId = message.MessageId;
             var indexOfCompany = message.Text.IndexOf("company") + 7;
-            var seachedCompany = message.Text.Substring(indexOfCompany, message.Text.Length - indexOfCompany);
-       
-           
+            var seachedCompany = message.Text.Substring(indexOfCompany+1, message.Text.Length - indexOfCompany-1);
 
-            client.SendTextMessageAsync(chatId, "Hello!", replyToMessageId: messageId);
+            Trace.TraceError("Searched Company" + seachedCompany);
+            var companiesVacancies = repository.GetVacanciesByCompanyName(seachedCompany);
+            client.SendTextMessageAsync(chatId, companiesVacancies.FirstOrDefault()?.VacancyUrl, replyToMessageId: messageId);
         }
     }
 }
--- JobsParser/Program.cs
+++ JobsParser/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JobsParser
 {
@@ -10,7 +6,7 @@ namespace JobsParser
     {
         static void Main(string[] args)
         {
-            string jsonData = System.IO.File.ReadAllText("./rabotaUa.json");
+            string jsonData = System.IO.File.ReadAllText(@"rabotaUa.json");
             var config = jsonData.ConvertJsonToClass<RabotaUaConfigEntity>();
             var parser = new RabotaUaParser(config);
             parser.GetJobsForAllPages();
